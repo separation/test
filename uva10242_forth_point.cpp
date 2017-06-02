927a7a246c19ae3557afa466cc59d3cbf49de8a1
separation <yw.amy.lin@gmail.com>
2017-06-02 18:23:31 +0800

    string to coordinate

--- uva10242_forth_point.cpp
+++ uva10242_forth_point.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <cmath>
 using namespace std;
+
+class coordinate
+{
+public:
+    coordinate(const string& str) : m_value(to_value(str))
+    {
+    }
+
+private:
+    int to_value(const string& str)
+    {
+        return stod(str) * pow(10, (int)PRECISION);
+    }
+
+private:
+    enum { PRECISION = 3 };
+    int m_value;
+};
 
 void test_case(const string& str)
 {
+    istringstream is(str);
+    string s;
+    while (is >> s)
+    {
+        coordinate c(s);
+    }
+
 }
 
 int main()
