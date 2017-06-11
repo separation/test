6748e1382ba04a6b15be3e2e486d06b8a3e0b484
separation <yw.amy.lin@gmail.com>
2017-06-11 18:48:01 +0800

    string to coordinates

--- uva10242_forth_point.cpp
+++ uva10242_forth_point.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 void test_case(const string& str)
 {
+    vector<double> coordinates{istream_iterator<double>{istringstream(str)}, istream_iterator<double>{}};
 }
 
 int main()
