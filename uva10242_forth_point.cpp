c3bb1926f6e3acabfe69de46a6e6dcee27b85dbd
separation <yw.amy.lin@gmail.com>
2017-06-13 00:19:37 +0800

    temp

--- uva10242_forth_point.cpp
+++ uva10242_forth_point.cpp
@@ -2,11 +2,26 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <array>
 using namespace std;
+
+template<size_t DIMENSION>
+class point
+{
+public:
+    friend istream& operator>>(istream& is, point<DIMENSION>& p)
+    {
+        p.m_coordinates = array<double, DIMENSION>{};
+        return is;
+    }
+
+private:
+    array<double, DIMENSION> m_coordinates;
+};
 
 void test_case(const string& str)
 {
-    vector<double> coordinates{istream_iterator<double>{istringstream(str)}, istream_iterator<double>{}};
+    vector<point<2>> coordinates{ istream_iterator<point<2>>{istringstream(str)}, istream_iterator<point<2>>{} };
 }
 
 int main()
