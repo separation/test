7979140e599129cec206bbde28a3a27411eec041
amy_lin <yw.amy.lin@gmail.com>
2017-06-17 14:50:02 +0800

    read world boundary

--- uva118_flatworld_explorers.cpp
+++ uva118_flatworld_explorers.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class robot_commander
+{
+public:
+    robot_commander(int x, int y) : m_x_bound(x), m_y_bound(y)
+    {
+    }
+
+private:
+    int m_x_bound;
+    int m_y_bound;
+};
 int main()
 {
+    int x = 0, y = 0;
+    cin >> x >> y;
+    robot_commander commander(x, y);
     return 0;
 }
