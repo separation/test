05a22db8626f649f7ffac33efa0c7f6e32faaa77
separation <yw.amy.lin@gmail.com>
2017-06-19 23:32:41 +0800

    read world bound

--- uva118_flatworld_explorers.cpp
+++ uva118_flatworld_explorers.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class world
+{
+public:
+    world(int x, int y) : m_x_bound(x), m_y_bound(y)
+    {}
+
+private:
+    int m_x_bound;
+    int m_y_bound;
+};
 int main()
 {
+    int x = 0, y = 0;
+    cin >> x >> y;
+    world my_world(x, y);
     return 0;
 }
