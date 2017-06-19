cd37015c37cccc14741e5ff4cd14887e6553ad63
separation <yw.amy.lin@gmail.com>
2017-06-19 23:46:09 +0800

    input robot

--- uva118_flatworld_explorers.cpp
+++ uva118_flatworld_explorers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class world
@@ -11,10 +12,32 @@ private:
     int m_x_bound;
     int m_y_bound;
 };
+
+class robot
+{
+public:
+    robot(int x, int y, char direction) : m_x(x), m_y(y), m_direction(direction)
+    {
+    }
+
+private:
+    int m_x;
+    int m_y;
+    char m_direction;
+};
+
 int main()
 {
     int x_bound = 0, y_bound = 0;
     cin >> x_bound >> y_bound;
     world my_world(x_bound, y_bound);
+
+    char direction = '\0';
+    string commands;
+    int x = 0, y = 0;
+    while (cin >> x >> y >> direction >> commands)
+    {
+        robot my_robot(x, y, direction);
+    }
     return 0;
 }
