44f07b9627112837222d542d9d1565d6d646a9a9
separation <yw.amy.lin@gmail.com>
2017-06-21 06:52:31 +0800

    input each robot

--- uva118_flatworld_explorers.cpp
+++ uva118_flatworld_explorers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class world
@@ -16,5 +17,11 @@ int main()
     int x = 0, y = 0;
     cin >> x >> y;
     world my_world(x, y);
+
+    char direction = '\0'; string commands;
+    while (cin >> x >> y >> direction >> commands)
+    {
+    }
+
     return 0;
 }
