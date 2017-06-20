a388cc714df305100e16455ec2efaa914cdcc686
separation <yw.amy.lin@gmail.com>
2017-06-20 21:29:23 +0800

    implement robot commands

--- uva118_flatworld_explorers.cpp
+++ uva118_flatworld_explorers.cpp
@@ -8,6 +8,10 @@ public:
     world(int x_bound, int y_bound) : m_x_bound(x_bound), m_y_bound(y_bound)
     {}
 
+    bool exist(int x, int y) const
+    {
+        return x <= m_x_bound && y <= m_y_bound;
+    }
 private:
     int m_x_bound;
     int m_y_bound;
@@ -20,10 +24,99 @@ public:
     {
     }
 
+    bool command(const world& w, char c)
+    {
+        switch (c)
+        {
+        case 'F':
+            return move_forward(w);
+        case 'R':
+            return turn_right();
+        case 'L':
+            return turn_left();
+        }
+
+        return false;
+    }
+
     friend ostream& operator<<(ostream& os, const robot& r)
     {
         os << r.m_x << " " << r.m_y << " " << r.m_direction << (r.m_alive ? "" : " LOST");
         return os;
+    }
+
+private:
+    bool move_forward(const world& w)
+    {
+        int x = m_x, y = m_y;
+        switch (m_direction)
+        {
+        case 'N':
+            y++;
+            break;
+        case 'E':
+            x++;
+            break;
+        case 'S':
+            y--;
+            break;
+        case 'W':
+            x--;
+            break;
+        default:
+            return false;
+        }
+
+        m_alive = w.exist(x, y);
+        if (m_alive)
+        {
+            m_x = x;
+            m_y = y;
+        }
+
+        return m_alive;
+    }
+
+    bool turn_right()
+    {
+        switch (m_direction)
+        {
+        case 'N':
+            m_direction = 'E';
+            return true;
+        case 'E':
+            m_direction = 'S';
+            return true;
+        case 'S':
+            m_direction = 'W';
+            return true;
+        case 'W':
+            m_direction = 'N';
+            return true;
+        }
+
+        return false;
+    }
+
+    bool turn_left()
+    {
+        switch (m_direction)
+        {
+        case 'N':
+            m_direction = 'W';
+            return true;
+        case 'E':
+            m_direction = 'N';
+            return true;
+        case 'S':
+            m_direction = 'E';
+            return true;
+        case 'W':
+            m_direction = 'S';
+            return true;
+        }
+
+        return false;
     }
 
 private:
@@ -38,6 +131,10 @@ class robot_commander
 public:
     void send_robot(const world& w, const string& commands, robot& r)
     {
+        for (const char &c : commands)
+        {
+            if (!r.command(w, c)) break;
+        }
     }
 };
 
